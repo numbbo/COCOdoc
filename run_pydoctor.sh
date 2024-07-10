76e375e892553e0273e8f9c46d7c3ca5b04124b3
nikohansen <nikolaus.hansen@inria.fr>
2024-07-11 01:32:14 +0200

    amend run_pydoctor.sh script for new repository

--- run_pydoctor.sh
+++ run_pydoctor.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # first argument is path/to/coco
 pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/cocopp' "$1"/code-postprocessing/cocopp
+touch "$1"/code-experiments/build/python/example/__init__.py
+pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/example' "$1"/code-experiments/build/python/example
+# after checking: apidocs/example/index.html
+# git add apidocs/example
+
+# the following requires to have run the build for cocoex via scripts/fabricate (maybe from the folder code-postprocessing)
 mkdir tmp
-cp -r "$1"/code-experiments/build/python/python tmp/cocoex
+cp -r "$1"/code-experiments/build/python/src/cocoex tmp
 pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/cocoex' tmp/cocoex
 rm -r tmp
-pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/example' "$1"/code-experiments/build/python/example/example_experiment2.py
-pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/example' "$1"/code-experiments/build/python/example/example_experiment_complete.py
+
+# replaced by the above --html-output='apidocs/example'
+# pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/example' "$1"/code-experiments/build/python/example/example_experiment2.py
